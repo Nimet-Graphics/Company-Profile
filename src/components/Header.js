7633e6e62bafd31d1db72421ea48547f0e04f255
Nimet Graphics <nimetgraphics@gmail.com>
2024-08-14 09:10:57 +0300

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
     <header className='fixed top-0 left-0 right-0 bg-white border-b shadow-md z-50'>
       <nav className=' container flex justify-between items-center px-8 py-5 mx-auto'>
         <Link to='/' alt='company-logo'>
-          <img src={logo} alt='Nimet-Graphics' className='h-6 w-9 ml-12 mt-1' />
+          <img src={logo} alt='Nimet-Graphics' className='h-6 w-9 ml-4 md:ml-8 mt-1' />
         </Link>
 
         {/* Hamburger Icon for mobile view */}
@@ -24,7 +24,7 @@ const Header = () => {
 
         {/* Navigation Links */}
         <ul
-          className={`flex flex-col md:flex-row md:space-x-6 font-bold font-roboto absolute md:static bg-white top-full left-0 w-full md:w-auto md:bg-transparent transition-all duration-500 ease-in-out ${
+          className={`flex flex-col md:flex-row md:space-x-6 pl-10 md:pl-0 font-bold font-roboto absolute md:static bg-white top-full left-0 w-full md:w-auto md:bg-transparent transition-all duration-500 ease-in-out ${
             isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
           } md:opacity-100 md:visible`}
         >
